8a292b600557c226274a635341ca78ee9e8d7b69
utkarshh1506 <utksin9621@gmail.com>
2025-07-09 22:00:18 +0530

    Resume Added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import './Hero.css'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import final from '../../assets/final.png'
+import React from "react";
+import "./Hero.css";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import final from "../../assets/final.png";
 
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
-        <img src={final} alt="" />
-        <h1><span>I'm Utkarsh Singh, </span>A Full Stack Developer.</h1>
-        <p>I am a Full Stack Developer with experience in building dynamic and responsive web applications from front to back. I work with technologies like JavaScript, React, Node.js, Express, and MongoDB to develop scalable solutions. I enjoy solving real-world problems, writing clean and maintainable code, and continuously learning new tools and frameworks to improve my craft.</p>
-        <div className='hero-action'>
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-            <div className="hero-resume">My Resume</div>
+    <div id="home" className="hero">
+      <img src={final} alt="" />
+      <h1>
+        <span>I'm Utkarsh Singh, </span>A Full Stack Developer.
+      </h1>
+      <p>
+        I am a Full Stack Developer with experience in building dynamic and
+        responsive web applications from front to back. I work with technologies
+        like JavaScript, React, Node.js, Express, and MongoDB to develop
+        scalable solutions. I enjoy solving real-world problems, writing clean
+        and maintainable code, and continuously learning new tools and
+        frameworks to improve my craft.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
         </div>
+        <div className="hero-resume">
+          <a
+            href="https://drive.google.com/file/d/1PFNWv__f7jZxpPNlCo0wyv-8LpVsFZLM/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="anchor-link"
+          >
+            My Resume
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
